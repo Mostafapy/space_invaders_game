03d79171763109697b483e77003d659c92989c35
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-26 15:17:04 +0200

    adding game loop to main file

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import pygame
+import random
+from game_loop import game_loop
+from PlayerOrEnemy import PlayerOrEnemy
 
-#Intialize pygame
-pygame.display.init()
+if __name__ == '__main__':
+    #Intialize pygame
+    pygame.display.init()
 
-# Create a screen
-# Open a window on the screen
-screen_width=800
-screen_height=600
-screen = pygame.display.set_mode([screen_width,screen_height])
+    # Create a screen
+    # Open a window on the screen
+    screen_width=800
+    screen_height=600
+    screen = pygame.display.set_mode([screen_width,screen_height])
 
-# Title and icon
-pygame.display.set_caption("Space Invadors")
-icon = pygame.image.load('ufo.png')
-pygame.display.set_icon(icon)
+    # Title and icon
+    pygame.display.set_caption("Space Invadors")
+    icon = pygame.image.load('ufo.png')
+    pygame.display.set_icon(icon)
 
-# Game Loop
-running = True
-while running:
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-           running = False
+    # Player
+    player = PlayerOrEnemy(pygame, "space-invaders.png", 370, 480)
 
-    #RGB RED, GREEN, BLUE
-    screen.fill((0, 0, 0))
-    pygame.display.update()
+    # Enemy
+    enemy_position_x = random.randint(0, 800)
+    enemy_position_y = random.randint(50, 150)
+    enemy = PlayerOrEnemy(pygame, "enemy.png", enemy_position_x, enemy_position_y)
+
+    # Game Loop
+    game_loop(pygame, screen, player, enemy)

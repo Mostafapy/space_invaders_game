aeb28209769df7bfea03440294269e58ea1af570
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-26 13:00:05 +0200

    adding key controls action to game_loop

--- game_loop.py
+++ game_loop.py
@@ -1,6 +1,9 @@
 def game_loop(pygame, screen, player):
     # Game Loop
     running = True
+
+    playerx_change = 0
+
     while running:
         #RGB RED, GREEN, BLUE
         screen.fill((0, 0, 0))
@@ -8,8 +11,18 @@ def game_loop(pygame, screen, player):
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
              running = False
+          
+          #Check keystroke pressed is right or left
+          if event.type == pygame.KEYDOWN:
+              if event.key == pygame.K_LEFT:
+                playerx_change = -0.3
+              if event.key == pygame.K_RIGHT:
+                playerx_change = 0.3
+          elif event.type == pygame.KEYUP:
+              if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                 playerx_change = 0
 
-        
+        player.player_x += playerx_change
         player.add_player(screen)
 
         pygame.display.update()

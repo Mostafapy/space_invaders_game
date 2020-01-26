1bc944f2bb08fc31fa8462a496788441558cbe84
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-26 12:25:22 +0200

    add player to the game loop

--- game_loop.py
+++ game_loop.py
@@ -1,11 +1,15 @@
-def game_loop(pygame, screen):
+def game_loop(pygame, screen, player):
     # Game Loop
     running = True
     while running:
+        #RGB RED, GREEN, BLUE
+        screen.fill((0, 0, 0))
+
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
              running = False
 
-        #RGB RED, GREEN, BLUE
-        screen.fill((0, 0, 0))
+        
+        player.add_player(screen)
+
         pygame.display.update()

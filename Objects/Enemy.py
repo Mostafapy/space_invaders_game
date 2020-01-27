0760165fa5f7d6835f17e34b70bbf631407caafe
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-27 18:58:51 +0200

    add object_movement method to Player and Enemy game object class

--- Objects/Enemy.py
+++ Objects/Enemy.py
@@ -1,4 +1,17 @@
-from GameObject import GameObject
+from Objects.GameObject import GameObject
 class Enemy(GameObject):
-    def add_object(self, screen):
-        screen.blit(self.game_object_img, (self.game_object_x, self.game_object_y))
+    def add_object(self, screen, player_x_pos= None, bullet_state = None):
+        if player_x_pos is None and bullet_state is None:
+           screen.blit(self.game_object_img, (self.game_object_x, self.game_object_y))
+
+    def object_movement(self, enemy_x_change, enemy_y_change):
+        # Add game boundries for enemy
+        self.game_object_x += enemy_x_change.value
+
+        if self.game_object_x <= 0:
+            enemy_x_change.value = 4
+            self.game_object_y += enemy_y_change.value
+
+        if self.game_object_x >= 736:
+            enemy_x_change.value = -4
+            self.game_object_y += enemy_y_change.value
--- Objects/Player.py
+++ Objects/Player.py
@@ -1,4 +1,15 @@
-from GameObject import GameObject
+from Objects.GameObject import GameObject
 class Player(GameObject):
-    def add_object(self, screen):
-        screen.blit(self.game_object_img, (self.game_object_x, self.game_object_y))
+
+    def add_object(self, screen, player_x_pos= None, bullet_state = None):
+        if player_x_pos is None and bullet_state is None:
+            screen.blit(self.game_object_img, (self.game_object_x, self.game_object_y))
+
+    def object_movement(self, player_x_change):
+         # Add game boundries for player
+        self.game_object_x+= player_x_change.value
+        
+        if self.game_object_x <= 0:
+           self.game_object_x = 0
+        elif self.game_object_x >= 736:
+            self.game_object_x = 736

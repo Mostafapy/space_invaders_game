a63cae9f5f183aad045b26e39ae3941c894170d2
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-27 18:57:06 +0200

    add object_movement abstract method to GameObject and add meta abstract

--- Objects/GameObject.py
+++ Objects/GameObject.py
@@ -1,15 +1,15 @@
-class GameObject:
+from abc import ABCMeta, abstractmethod
+class GameObject(metaclass=ABCMeta):
     def __init__(self, pygame, game_object_img_path, postion_x, postion_y):
          # Player / Enemy / Bullet
-         self.game_object = pygame.image.load(game_object_img_path)
+         self.game_object_img = pygame.image.load(game_object_img_path)
          self.game_object_x = postion_x
          self.game_object_y = postion_y
-         super.__init__()
 
     @abstractmethod
-    def add_object(self, screen):
+    def add_object(self, screen, player_x_pos = None, bullet_state = None):
         pass
 
     @abstractmethod
-    def object_movement(self, **change_movement_params):
+    def object_movement(self, *change_movement_params):
         pass

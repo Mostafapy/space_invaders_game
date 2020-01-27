b575a67548d81884b9194bc3b684ce216ca586f5
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-27 18:58:02 +0200

    enhance main file with the new stucture

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
 import random
 from game_loop import game_loop
-from Player import Player
-from Enemy import Enemy
+from Objects.Player import Player
+from Objects.Enemy import Enemy
+from Objects.Bullets import Bullets
 
 if __name__ == '__main__':
     #Intialize pygame
@@ -15,20 +16,23 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode([screen_width,screen_height])
 
     #Background
-    background_img = pygame.image.load("background.png")
+    background_img = pygame.image.load("images/background.png")
 
     # Title and icon
     pygame.display.set_caption("Space Invadors")
-    icon = pygame.image.load("ufo.png")
+    icon = pygame.image.load("images/ufo.png")
     pygame.display.set_icon(icon)
 
     # Player
-    player = Player(pygame, "space-invaders.png", 370, 480)
+    player = Player(pygame, "images/space-invaders.png", 370, 480)
 
     # Enemy
     enemy_position_x = random.randint(0, 800)
     enemy_position_y = random.randint(50, 150)
-    enemy = Enemy(pygame, "enemy.png", enemy_position_x, enemy_position_y)
+    enemy = Enemy(pygame, "images/enemy.png", enemy_position_x, enemy_position_y)
+
+    #Bullets
+    bullet = Bullets(pygame, "images/bullet.png", 0, 480)
 
     # Game Loop
-    game_loop(pygame, screen, player, enemy, background_img)
+    game_loop(pygame, screen, player, enemy, bullet, background_img)

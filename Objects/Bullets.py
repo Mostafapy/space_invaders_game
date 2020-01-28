587b8455237fa486278a34495558ff6a9055aa4c
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-28 13:43:34 +0200

    add add_collision function to Bullets class

--- Objects/Bullets.py
+++ Objects/Bullets.py
@@ -1,3 +1,5 @@
+import math
+import random
 from Objects.GameObject import GameObject
 
 class Bullets(GameObject):
@@ -14,3 +16,19 @@ class Bullets(GameObject):
          if bullet_state.value is "fire":
              self.add_object(screen, bullet_state)
              self.game_object_y -= bullety_change.value
+
+     def is_collision(self, enemy_x, enemy_y):
+         distance = math.sqrt((math.pow(enemy_x - self.game_object_x, 2)) + (math.pow(enemy_y- self.game_object_y, 2)))
+         if distance < 27:
+             return True
+         else:
+             return False
+
+     def add_collision(self, enemy, bullet_state, score):
+         iscollide = self.is_collision(enemy.game_object_x, enemy.game_object_y)
+         if iscollide:
+             self.game_object_y = 480
+             bullet_state.value = "ready"
+             score.value += 1
+             enemy.game_object_x = random.randint(0, 735)
+             enemy.game_object_y= random.randint(50, 150)

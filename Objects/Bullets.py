d7893ad747d9e92ee35b2bbc54b65c4bb2e676b4
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-28 11:16:01 +0200

    add multiple bullets shooting

--- Objects/Bullets.py
+++ Objects/Bullets.py
@@ -1,12 +1,16 @@
 from Objects.GameObject import GameObject
 
 class Bullets(GameObject):
-     def add_object(self, screen, player_x_pos = None, bullet_state = None):
-        if player_x_pos is not None and bullet_state is not None:
+     def add_object(self, screen, bullet_state = None):
+        if bullet_state is not None:
            bullet_state.value = "fire"
-           screen.blit(self.game_object_img, (player_x_pos + 16, self.game_object_y + 10))
+           screen.blit(self.game_object_img, (self.game_object_x + 16, self.game_object_y + 10))
     
-     def object_movement(self, screen, player_x, bullety_change, bullet_state):
+     def object_movement(self, screen, bullety_change, bullet_state):
+         if self.game_object_y <= 0:
+            self.game_object_y = 480
+            bullet_state.value = "ready" 
+
          if bullet_state.value is "fire":
-             self.add_object(screen, player_x, bullet_state)
+             self.add_object(screen, bullet_state)
              self.game_object_y -= bullety_change.value

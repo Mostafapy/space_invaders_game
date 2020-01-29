dae125d3aacaac2b0ed5b1b346729da6516d0c75
MostafaHisham <mostafa.hisham@digi2.com>
2020-01-29 14:08:13 +0200

    fix add_object function pf Font class

--- Objects/Font.py
+++ Objects/Font.py
@@ -1,8 +1,8 @@
 from Objects.GameObject import GameObject
 
 class Font(GameObject):
-    def add_object(self, screen, score_value):
-        score = self.game_object_img_or_font.render("Score: " + str(score_value.value), True, (255, 128, 0))
+    def add_object(self, screen, text):
+        score = self.game_object_img_or_font.render(text, True, (255, 128, 0))
         screen.blit(score, (self.game_object_x, self.game_object_y))
 
     def object_movement(self, params = None):
